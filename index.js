03b26134d497da9eed252038965817567d72d739
mani <mani95lisa@gmail.com>
2014-10-14 18:48:50 +0800

    0.0.2

--- index.js
+++ index.js
@@ -2,28 +2,13 @@
     "use strict";
     var Card = {};
     Card.parse = function(postContent, callback) {
-        var re = /\[card.*?\]/gm;
-        var arr = postContent.match(re);
-        if(arr && arr.length == 1){
-            var config = arr[0];
-            config = config.replace(']', '');            
-            arr = config.split(':');
-            if(arr.length == 2){
-                arr = arr[1].split('|');
-                var cardInfo = arr[0];
-                var width = 400;
-                var height = 150;
-                if(arr.length == 2){
-                    arr = arr[1].split('*');
-                    if(arr.length == 2){
-                        width = arr[0];
-                        height = arr[1];
-                    }                    
-                }
-                var card = '<div class="github-card" data-github="'+cardInfo+'" data-width="'+width+'" data-height="'+height+'"></div><script src="/plugins/nodebb-plugin-github-card/static/widget.js"></script>'
-                postContent = postContent.replace(re, card);
-            }            
+        var re = /\[card:(.*?)(\b|\|(.*?)\*(.*)?)\]/gm;
+
+        if (postContent.match(re)) {
+            var card = '<div class="github-card" data-github="$1" data-width="$3" data-height="$4"></div><script src="/plugins/nodebb-plugin-github-card/static/widget.js"></script>'
+            postContent = postContent.replace(re, card);
         }
+
         callback(null, postContent);
     };
 
