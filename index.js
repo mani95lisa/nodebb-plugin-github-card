ec8f2219ce826f8d0ca3c1ca97cd76bef65acf55
Mani Wang <mani95lisa@gmail.com>
2015-07-31 10:00:57 +0800

    upgrade for nodebb 0.7.x

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 (function(module) {
     "use strict";
     var Card = {};
-    Card.parse = function(postContent, callback) {
+    Card.parse = function(data, callback) {
         var re = /\[card:(.*?)(\b|\|(.*?)\*(.*)?)\]/gm;
+        var postContent = data.postData.content;
 
         if (postContent.match(re)) {
             var card = '<div class="github-card" data-github="$1" data-width="$3" data-height="$4"></div><script src="/plugins/nodebb-plugin-github-card/static/widget.js"></script>'
             postContent = postContent.replace(re, card);
+            data.postData.content = postContent;
         }
 
-        callback(null, postContent);
+        callback(null, data);
     };
 
     module.exports = Card;
